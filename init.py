63b480e0b57f36cbfdb252af775a9de7f1b9537b
xosadmin <9164356+xosadmin@users.noreply.github.com>
2025-02-12 17:41:15 +0800

    Finalise init.py

--- init.py
+++ init.py
@@ -1,6 +1,15 @@
 import os
 from utils import runCommand
 from conf import sysconfig
+import platform
+
+def detectSys():
+    opera_sys = platform.system()
+    if opera_sys == "Linux":
+        dist = platform.linux_distribution()
+        return str(dist)
+    else:
+        return "Unknown"
 
 def initDependRun():
     dependencies = ["ping","traceroute","mtr"]
@@ -11,18 +20,37 @@ def initDependRun():
             notFoundDepends.append(item)
     if len(notFoundDepends) > 0:
         print(f"Error: Dependency {notFoundDepends} not satisfied.")
-        return False
+        return notFoundDepends
     else:
         return True
 
 def testFileGen():
     absLocation = os.path.join("testfile")
-    testFileSize = sysconfig["TestFileSize"]
+    testFileSize = ["10","100","1000"]
     if not os.path.exists(absLocation):
         os.makedirs(absLocation)
     for item in testFileSize:
+        print(f"Generating {item}MB.test")
         filename = os.path.join(absLocation, f"{item}MB.test")
         with open(filename, 'wb') as f:
             f.write(bytearray(int(item) * 1024 * 1024))
     return True
 
+os_sys = detectSys().lower()
+if os_sys != "unknown":
+    initdep = initDependRun()
+    if initdep:
+        testFileGen()
+        print("Complete.")
+    else:
+        for item in initdep:
+            if os_sys == "centos" or os_sys == "fedora" or os_sys == "almalinux" or os_sys == "rockylinux":
+                runCommand(["yum","-y","install",item])
+            elif os_sys == "ubuntu" or os_sys == "debian":
+                runCommand(["apt","-y","install",item])
+            else:
+                print("Cannot find suitable installation method(s). Exiting...")
+                exit(1)
+else:
+    print("Unsupported OS. Exiting...")
+    exit(1)
