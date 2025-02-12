da14385d1fb8c766e41b2a3acd550668d17df521
xosadmin <9164356+xosadmin@users.noreply.github.com>
2025-02-12 23:06:46 +0800

    Optimise GUI

--- conf.py
+++ conf.py
@@ -3,6 +3,6 @@ sysconfig = {
     "hostname": "Server",
     "Location": "",
     "pingtime": "4", # Maximum Ping times of each time
-    "iperf3": "Disabled", # Specify iperf3 server for speedtest, set Disable for disable iperf3
+    "iperf3": "Disabled", # Specify iperf3 server for speedtest, set to Disabled for disable iperf3
     "DiscourageSearchEngine": True # Set to True if you don't wish search engine index your LG
 }
--- static/main.js
+++ static/main.js
@@ -7,15 +7,31 @@ function copycontent(flag) {
 
 function doActions(type) {
     var value = document.getElementById("dstaddr").value;
-    document.getElementById("lgResult").style.display = "none";
-    const Http = new XMLHttpRequest();
-    const url='/process/' + type + "/" + value;
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = (e) => {
-        if (Http.readyState === 4 && Http.status === 200) {
-            document.getElementById("lgResult").innerHTML = Http.responseText;
-            document.getElementById("lgResult").style.display = "block";
+    if (value === "") {
+        document.getElementById("inputWarn").innerHTML = "<p>Error: The destination address cannot be empty!</p>";
+        document.getElementById("inputWarn").style.display = "block";
+    }
+    else {
+        document.getElementById("lgResult").style.display = "none";
+        document.getElementById("inputWarn").style.display = "none";
+        const Http = new XMLHttpRequest();
+        const url='/process/' + type + "/" + value;
+        Http.open("GET", url);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+            if (Http.readyState === 4 && Http.status === 200) {
+                document.getElementById("lgResult").innerHTML = Http.responseText;
+                document.getElementById("lgResult").style.display = "block";
+            }
         }
+    }
+}
+
+function hideshowresult(flag) {
+    if (flag === "hide") {
+        document.getElementById("lgResult").style.display = "none";
+    }
+    else {
+        document.getElementById("lgResult").style.display = "block";
     }
 }
