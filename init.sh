42399e316eaf46e8093085d0e5735e6f2da99963
xosadmin <9164356+xosadmin@users.noreply.github.com>
2025-02-12 18:41:26 +0800

    Reshape init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-cd /opt/axlg
-if [ ! -d testfile ]; then
-  mkdir -p testfile
-  cd testfile
-  dd if=/dev/zero of=10mb.test bs=1M count=10
-  dd if=/dev/zero of=100mb.test bs=1M count=100
-  dd if=/dev/zero of=1000mb.test bs=1M count=1000
-else
-  if [[ ! -f testfile/10mb.test ] || [ ! -f testfile/100mb.test ] || [ ! -f testfile/1000mb.test ]]; then
-    cd testfile
-    dd if=/dev/zero of=10mb.test bs=1M count=10
-    dd if=/dev/zero of=100mb.test bs=1M count=100
-    dd if=/dev/zero of=1000mb.test bs=1M count=1000
-  fi
+DIR="/opt/axlg"
+TESTFILE_DIR="$AXLG_DIR/testfile"
+if [ ! -d "$TESTFILE_DIR" ]; then
+  mkdir -p "$TESTFILE_DIR" || { echo "Error creating directory" >&2; exit 1; }
 fi
-cd /opt/axlg
-uwsgi --http :5000 --wsgi-file app.py --callable app
+
+create_test_file() {
+  local size="$1"
+  local filename="$2"
+  local filepath="$TESTFILE_DIR/$filename"
+
+  if [ ! -f "$filepath" ]; then
+    dd if=/dev/zero of="$filepath" bs=1M count="$size" || { echo "Error creating file $filename" >&2; exit 1; }
+  fi
+}
+
+create_test_file 10 10mb.test
+create_test_file 100 100mb.test
+create_test_file 1000 1000mb.test
+
+
+cd "$DIR" || { echo "Error changing directory" >&2; exit 1; }
+
+uwsgi --http :5000 --wsgi-file app.py --callable app || { echo "Error starting uwsgi" >&2; exit 1; }
